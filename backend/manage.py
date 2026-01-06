b7463f6c53ec427237f1d865f3ed5ef1847e6a73
Rayhq <sreekarthik24clt@gmail.com>
2026-01-07 01:26:14 +0530

    fix: load .env variables in manage.py script

--- backend/manage.py
+++ backend/manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 def main():
+    # Load .env file for local development
+    dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
