3f56d67e950331cd9b257e851de050975c4036e5
astraietm <astraietm25@gmail.com>
2026-01-10 13:13:29 +0530

    Remove decorative HUD border elements from MainLayout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -13,16 +13,7 @@ const MainLayout = () => {
       {/* <MatrixBackground /> Removed per request */}
       <BackgroundOrbs />
       
-      {/* Decorative Fixed HUD Elements */}
-      <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-50 p-6 hidden lg:block">
-          <div className="absolute top-6 left-6 w-8 h-8 border-l-2 border-t-2 border-primary/30"></div>
-          <div className="absolute top-6 right-6 w-8 h-8 border-r-2 border-t-2 border-primary/30"></div>
-          <div className="absolute bottom-6 left-6 w-8 h-8 border-l-2 border-b-2 border-primary/30"></div>
-          <div className="absolute bottom-6 right-6 w-8 h-8 border-r-2 border-b-2 border-primary/30"></div>
-          
-          <div className="absolute top-1/2 left-6 w-1 h-12 bg-primary/20 -translate-y-1/2"></div>
-          <div className="absolute top-1/2 right-6 w-1 h-12 bg-primary/20 -translate-y-1/2"></div>
-      </div>
+
       <Navbar />
       <main className="flex-grow">
         <Outlet />
