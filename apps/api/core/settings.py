f56e20ab1a53400773cfa47cb962f9e022ca3c18
cobrasanjay1 <sanjay.vinod25@gmail.com>
2026-01-26 12:48:23 +0530

    fix for zoho

--- apps/api/core/settings.py
+++ apps/api/core/settings.py
@@ -166,13 +166,13 @@ if not DEBUG:
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.environ.get('EMAIL_HOST2', 'smtp.zoho.com')  # Using Zoho Mail
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER2', 'contact@astraietm.in')  # Using Zoho
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Astra Events <noreply@astraietm.in>')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL2', 'ASTRA Events <contact@astraietm.in>')  # Using Zoho
 
 # Razorpay Configuration
 RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID', '')
